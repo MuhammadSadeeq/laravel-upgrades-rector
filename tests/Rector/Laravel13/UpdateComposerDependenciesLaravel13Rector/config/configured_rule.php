022f4fa86de06d561dce01262b8d97a654e50fb3
Muhammad Sadeeq <afridi.sadeeq.m@gmail.com>
2026-08-23 15:22:12 +0500

    build: require Rector ^2.3 and add console, process, semver deps

--- tests/Rector/Laravel13/UpdateComposerDependenciesLaravel13Rector/config/configured_rule.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use MuhammadSadeeq\LaravelUpgradesRector\Rector\Laravel13\UpdateComposerDependenciesLaravel13Rector;
-use Rector\Config\RectorConfig;
-
-return RectorConfig::configure()->withRules([UpdateComposerDependenciesLaravel13Rector::class]);
